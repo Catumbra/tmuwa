87fff3998da09181e8a67f485d86644e9322fc28
catumbra <41587596+catumbra@users.noreply.github.com>
2018-08-01 00:42:56 +0900

    Added style to input area
    1. Added style to ".streamerInput"
    2. Reconstructed style.css code and index.html tags

---
+++ streamerUnit.js
@@ -0,0 +1,28 @@
+function getPlayerUrl(streamerID)
+{
+    var url = "https://player.twitch.tv/?channel=" + streamerID;
+    return url;
+}
+function getChatUrl(streamerID)
+{
+    var url = "https://www.twitch.tv/embed/" + streamerID + "/chat";
+    return url;
+}
+function modifyStreamerSet(self)
+{
+    thisStreamerUnit = self.parentNode.parentNode;
+    targetStreamerSet = thisStreamerUnit.querySelector(".streamerSet");
+
+    streamerID = thisStreamerUnit.querySelector(".streamerInput").querySelector(".textInput").value;
+
+    targetStreamerSet.querySelector(".twitchPlayer").src = getPlayerUrl(streamerID);
+    targetStreamerSet.querySelector(".twitchChat").src = getChatUrl(streamerID);
+    
+    // targetStreamerSet = self.parentNode.parentNode.querySelector('.streamerSet');
+    // targetIframes = targetStreamerSet.querySelectorAll('iframe');
+
+    // streamerID = self.parentNode.querySelector('input').value;
+
+    // targetIframes[0].src = getPlayerUrl(streamerID);
+    // targetIframes[1].src = getChatUrl(streamerID);
+}
