fd623c0637da071509f33f8a564a5f87ac51cfdc
catumbra <41587596+catumbra@users.noreply.github.com>
2018-08-14 22:12:59 +0900

    Make it easy to use input tags

---
+++ interactives.js
@@ -0,0 +1,44 @@
+function getPlayerUrl(streamerID)
+{
+    var url = "https://player.twitch.tv/?channel=" + streamerID;
+    return url;
+}
+function getChatUrl(streamerID)
+{
+    var url = "https://www.twitch.tv/embed/" + streamerID + "/chat";
+    return url;
+}
+function modifyStreamerSet(self)
+{
+    thisStreamerUnit = self.parentNode.parentNode;
+    targetStreamerSet = thisStreamerUnit.querySelector(".streamerSet");
+
+    streamerID = thisStreamerUnit.querySelector(".unitTopBar").querySelector(".textInput").value;
+
+    targetStreamerSet.querySelector(".twitchPlayer").src = getPlayerUrl(streamerID);
+    targetStreamerSet.querySelector(".twitchChat").src = getChatUrl(streamerID);
+}
+function removeStreamerUnit(self)
+{
+    targetNode = self.parentNode.parentNode;
+    if(targetNode.className === "streamerUnit")
+        targetNode.remove();
+    else
+        alert(targetNode + "is not streamerUnit");
+}
+function addUnit()
+{
+    fetch("streamerUnit_default.html")
+    .then(function(response){
+        response.text().then(function(text){
+            $("#streamerUnitsHolder").append(text);
+        })
+    })
+}
+
+// Triggers buttonInput when press ENTER on textInput
+function functionIfEnter(self, e)
+{
+    if(e.keyCode === 13)
+        self.parentNode.querySelector(".buttonInput").click();
+}
