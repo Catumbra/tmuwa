fffe87fb95259a398fb4bf26c586ae32ca720e66
catumbra <41587596+catumbra@users.noreply.github.com>
2018-08-30 22:23:24 +0900

    Added chat toggle, Refactoring, Deleted narrow window mode
    v0.2.1
    Refactory:
    -Changed buttonAdd in a reasonable way
    -Add getParentStreamerUnit() function
    Added Feature:
    -Added Chat toggle button
    Deleted:
    -Temporary deleted auto adjustment feature in narrow window.

--- interactives.js
+++ interactives.js
@@ -8,15 +8,43 @@ function getChatUrl(streamerID)
     var url = "https://www.twitch.tv/embed/" + streamerID + "/chat";
     return url;
 }
+function getParentStreamerUnit(self)
+{
+    var currentNode = self;
+    while(true)
+    {
+        currentNode = currentNode.parentNode;
+        if(currentNode.className === "streamerUnit")
+            return currentNode;
+        else if(currentNode.tagName === "BODY")
+            return 0;
+    }
+}
 function modifyStreamerSet(self)
 {
-    thisStreamerUnit = self.parentNode.parentNode;
-    targetStreamerSet = thisStreamerUnit.querySelector(".streamerSet");
+    var thisStreamerUnit = self.parentNode.parentNode;
+    var targetStreamerSet = thisStreamerUnit.querySelector(".streamerSet");
 
-    streamerID = thisStreamerUnit.querySelector(".unitTopBar").querySelector(".textInput").value;
+    var streamerID = thisStreamerUnit.querySelector(".unitTopBar").querySelector(".textInput").value;
 
     targetStreamerSet.querySelector(".twitchPlayer").src = getPlayerUrl(streamerID);
     targetStreamerSet.querySelector(".twitchChat").src = getChatUrl(streamerID);
+}
+function toggleChat(self)
+{
+    var thisStreamerUnit = getParentStreamerUnit(self);
+    var thisStreamerSet = thisStreamerUnit.querySelector(".streamerSet");
+    var targetChatNode = thisStreamerSet.querySelector(".twitchChat");
+    if(window.getComputedStyle(targetChatNode).getPropertyValue('display') === 'none')
+    {
+        targetChatNode.style.display = 'inline';
+        thisStreamerSet.style.gridTemplateColumns = '1fr 350px';
+    }
+    else
+    {
+        targetChatNode.style.display = 'none';
+        thisStreamerSet.style.gridTemplateColumns = '1fr';
+    }
 }
 function removeStreamerUnit(self)
 {
