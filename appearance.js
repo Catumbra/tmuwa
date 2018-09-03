e834384dc15e30c0f6a9b133f958bd1d942d8d31
catumbra <41587596+catumbra@users.noreply.github.com>
2018-09-03 15:43:38 +0900

    Refactory: deleted unnecessary code in index.html
    -Deleted identical code from streamerUnt_default.html in index.html.
    -Added window.onload code in appearance.js

--- appearance.js
+++ appearance.js
@@ -1,9 +1,18 @@
 // Getting version values and put them inside footer
-fetch('version').then(function(response) {
-    response.text().then(function(text) {
-        var targetPTag = document.querySelector("#versionParagraph");
-        var items = text.split('\n');
-        targetPTag.innerHTML = items[0];
-        targetPTag.setAttribute("title", "Last Update: " + items[1].replace("###### ", ""));
-    })
-});
+function getAndWriteVersion() 
+{
+    fetch('version').then(function(response) {
+        response.text().then(function(text) {
+            var targetPTag = document.querySelector("#versionParagraph");
+            var items = text.split('\n');
+            targetPTag.innerHTML = items[0];
+            targetPTag.setAttribute("title", "Last Update: " + items[1].replace("###### ", ""));
+        })
+    });
+}
+
+// Run when page is loaded
+window.onload = function() {
+    addUnit();
+    getAndWriteVersion();
+}
