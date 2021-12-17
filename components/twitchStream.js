210d1d0d0eabc0e1e08e998db394ac0ab49a943b
catumbra <deukrayx@gmail.com>
2021-12-17 01:15:45 +0000

    트위치 스트림이 2개인 환경 테스트 + 디자인 tweak

--- components/twitchStream.js
+++ components/twitchStream.js
@@ -1,32 +1,16 @@
-const template = document.createElement('template');
-template.innerHTML = `
-    <style>
-        #twitch-embed {
-            height: 100%;
-        }
-    </style>
-    <div class="control-bar"></div>
-    <div id="twitch-embed"></div>
-`;
+// Twitch Embed를 이용하기 위해 twitch-stream 내부로 접근해야 하므로 Shadow DOM 사용 불가
 
 class TwitchStream extends HTMLElement {
     constructor() {
         super();
 
-        // this.attachShadow({mode: 'open'});
-        // this.shadowRoot.appendChild(template.content.cloneNode(true));
-
+        this.twitchEmbedID = uuidv4();
         this.channel = this.getAttribute('channel');
         this.isDarkMode = true;
         this.isChatOn = true;
         this.innerHTML = `
-        <style>
-            #twitch-embed {
-                height: 100%;
-            }
-        </style>
-        <div class="control-bar"></div>
-        <div id="twitch-embed"></div>
+        <div class="control-bar">control-bar</div>
+        <div class="twitch-embed" id="${this.twitchEmbedID}"></div>
         `;
 
         this.addStream();
@@ -41,14 +25,9 @@ class TwitchStream extends HTMLElement {
         embedOptions['height'] = '100%';
         embedOptions['channel'] = this.channel;
         embedOptions['parent'] = window.location.hostname;
+        embedOptions['muted'] = true; // 테스트중 (임시)
 
-        new Twitch.Embed("twitch-embed", embedOptions);
-        // new Twitch.Embed("twitch-embed", {
-        //     width: "100%",
-        //     height: "100%",
-        //     channel: "jungtaejune",
-        //     parent: window.location.hostname
-        // });
+        new Twitch.Embed(this.twitchEmbedID, embedOptions);
     }
     addDummyStream() {
 
@@ -56,4 +35,3 @@ class TwitchStream extends HTMLElement {
 }
 
 window.customElements.define('twitch-stream', TwitchStream);
-
