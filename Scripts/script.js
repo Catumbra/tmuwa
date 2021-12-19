c4c28f2069bb0311d313dac6f0f7122c30c59835
catumbra <deukrayx@gmail.com>
2021-12-19 08:06:35 +0000

    twitch-stream 추가하는 함수 & Layout 바꾸는 함수 추가

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,28 @@
 // uuid 생성기 코드
 function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-  }
-  
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+    }
+
+// twitch-stream 갯수
+function countStreams() {
+    var streamsContainer = document.getElementById('streams-container');
+    var count = streamsContainer.getElementsByTagName('twitch-stream').length;
+    return count;
+}
+
+// twitch-stream 레이아웃 자동 변경
+// TODO 주변 환경에 따라 자동으로 레이아웃 계산하는 알고리즘 추가
+function setStreamLayout() {
+    var nStreams = countStreams();
+    var container = document.getElementById("streams-container");
+    container.style.gridTemplateColumns = `repeat(${nStreams}, 1fr)`;
+}
+
+function addStream() {
+    var container = document.getElementById("streams-container");
+    container.appendChild(new TwitchStream());
+
+    setStreamLayout();
+}
--- components/twitchStream.js
+++ components/twitchStream.js
@@ -1,5 +1,4 @@
-// Twitch Embed를 이용하기 위해 twitch-stream 내부로 접근해야 하므로 Shadow DOM 사용 불가
-
+// TODO 트위치 스트림 remove 하는 기능, 버튼 이벤트 리스너 추가 및 제거
 class TwitchStream extends HTMLElement {
     constructor() {
         super();
@@ -9,11 +8,22 @@ class TwitchStream extends HTMLElement {
         this.isDarkMode = true;
         this.isChatOn = true;
         this.innerHTML = `
-        <div class="control-bar">control-bar</div>
+        <div class="control-bar">
+            <button>Close</button>
+        </div>
         <div class="twitch-embed" id="${this.twitchEmbedID}"></div>
         `;
 
-        this.addStream();
+        if (this.channel == null) {
+            this.addDummyStream();
+        } else {
+            this.addStream();
+        }
+
+        // var btn_DEBUG = document.createElement("button");
+        // btn_DEBUG.innerText = "DEBUG";
+        // btn_DEBUG.setAttribute("onClick", this.removeStream());
+        // this.getElementsByClassName("control-bar")[0].appendChild(btn_DEBUG);
     }
 
     setChannel(channel) {
@@ -30,7 +40,16 @@ class TwitchStream extends HTMLElement {
         new Twitch.Embed(this.twitchEmbedID, embedOptions);
     }
     addDummyStream() {
+        // TODO channel 입력하는 란 추가, 입력시 twitch-embed로 변환하는 기능
+        var dummyStreamHTML = `
+        <div style="background-color: #18181a; height: 100%; width: 100%;">
 
+        </div>
+        `;
+        this.getElementsByClassName("twitch-embed")[0].innerHTML = dummyStreamHTML;
+    }
+    removeStream() {
+        this.remove();
     }
 }
 
